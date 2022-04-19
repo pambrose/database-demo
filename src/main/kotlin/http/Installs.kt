09e0c6d8887edf13cd99522d915daa3d9e77ecaf
pambrose <pambrose@mac.com>
2022-04-18 23:09:43 -0700

    Upgrade jars

--- src/main/kotlin/http/Installs.kt
+++ src/main/kotlin/http/Installs.kt
@@ -4,10 +4,12 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.locations.*
-import io.ktor.server.plugins.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import org.slf4j.event.Level
+import org.slf4j.event.*
 
 fun Application.installs() {
   install(Compression) {
@@ -30,8 +32,7 @@ fun Application.installs() {
     exitCodeSupplier = { 0 } // ApplicationCall.() -> Int
   }
 
-  install(Locations) {
-  }
+  install(Locations)
 
   install(StatusPages) {
     exception<AuthenticationException> { call, _ ->
